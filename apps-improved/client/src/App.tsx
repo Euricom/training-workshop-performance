4eb9fb238a2e167917bbae89e92e1a264ae7cfdb
Arne <arne.goolaerts@resengo.com>
2024-03-07 15:09:10 +0100

    Add improved version of calculator

--- apps-improved/client/src/App.tsx
+++ apps-improved/client/src/App.tsx
@@ -1,27 +1,25 @@
 import { Navbar } from "./Navbar";
 import Home from "./pages/Home.tsx";
 import "./index.css";
-import {
-  BrowserRouter,
-  Route,
-  Routes,
-} from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Products from "./pages/Products.tsx";
 import BigDate from "./pages/BigDate.tsx";
+import Calculator from "./pages/Calculator.tsx";
 
-const App = () =>  {
+const App = () => {
   return (
     <>
-     <BrowserRouter>
-      <Navbar/>
+      <BrowserRouter>
+        <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/products" element={<Products />} />
           <Route path="/bigdate" element={<BigDate />} />
+          <Route path="/calculator" element={<Calculator />} />
         </Routes>
-    </BrowserRouter>
+      </BrowserRouter>
     </>
   );
-}
+};
 
 export default App;
--- apps-improved/client/src/Navbar.tsx
+++ apps-improved/client/src/Navbar.tsx
@@ -1,14 +1,14 @@
-import React from 'react'
+import React from "react";
 import { Link } from "react-router-dom";
 export const Navbar = () => {
   return (
-    <nav className='flex gap-2'>
+    <nav className="flex gap-2">
       <Link to="/">Home</Link>
       <Link to="/products">Products</Link>
-      <Link to="/markdown">Markdown</Link>
-      <Link to="/bigdate">BigDate</Link>
+      <Link to="/bigdate">Bigdate</Link>
       <Link to="/calculator">Calculator</Link>
+      <Link to="/markdown">Markdown</Link>
       <Link to="/users">Users</Link>
-</nav>
-  )
-}
+    </nav>
+  );
+};
