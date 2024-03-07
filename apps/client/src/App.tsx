b00ce6ceee973c3282c512f09a2f47f332368132
Arne <arne.goolaerts@resengo.com>
2024-03-07 13:31:15 +0100

    Added bigdate page with moment package

--- apps/client/src/App.tsx
+++ apps/client/src/App.tsx
@@ -7,19 +7,20 @@ import {
   Routes,
 } from "react-router-dom";
 import Products from "./pages/Products.tsx";
+import BigDate from "./pages/BigDate.tsx";
 
 const App = () =>  {
   return (
-    <>
+    <div className="p-2">
      <BrowserRouter>
       <Navbar/>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/products" element={<Products />} />
-          <Route path="/bigdate" element={<Products />} />
+          <Route path="/bigdate" element={<BigDate />} />
         </Routes>
     </BrowserRouter>
-    </>
+    </div>
   );
 }
 
--- apps/client/src/pages/BigDate.tsx
+++ apps/client/src/pages/BigDate.tsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import moment from 'moment'
 
 const BigDate = () => {
+const formattedDate = moment().format('MMMM Do YYYY, h:mm:ss a');
+
   return (
-    <div>BigDate</div>
+  <div>
+    <h1 className="text-2xl">Big Date</h1>
+    <p>{formattedDate}</p>
+  </div>
   )
 }
 
