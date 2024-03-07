625540811db3cd266ce490c23f2690f1a37e1396
Arne <arne.goolaerts@resengo.com>
2024-03-07 10:45:07 +0100

    Add navbar

--- apps/client/src/main.tsx
+++ apps/client/src/main.tsx
@@ -1,27 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
-import "./index.css";
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
-import Products from "./pages/Products.tsx";
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-  },
-  {
-    path: "/products",
-    element: <Products />,
-  },
-]);
+
 
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+     <App />
   </React.StrictMode>,
 );
---
+++ apps/client/src/pages/Home.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import home from '/home.jpg'
+
+const Home = () => {
+    return (
+        <>
+          <h1 className="text-2xl"> Performance</h1>
+          <img alt="home image" src={home} />
+        </>
+      );
+}
+
+export default Home
